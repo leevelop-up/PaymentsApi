ca8af79bfdb0705e4a536a9b16a7f22d24cc32b1
LeeDonghyun <63052631+leevelop-up@users.noreply.github.com>
2023-10-31 23:29:17 +0900

    push 파일

--- src/main/java/com/example/paymentsapi/PaymentsApiApplication.java
+++ src/main/java/com/example/paymentsapi/PaymentsApiApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class PaymentsApiApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(PaymentsApiApplication.class, args);
 	}
