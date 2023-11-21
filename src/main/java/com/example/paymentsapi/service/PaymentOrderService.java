5a4306f0c6b215aaaf669e788c8a41a5665eed69
LeeDonghyun <63052631+leevelop-up@users.noreply.github.com>
2023-11-21 23:27:22 +0900

    Service 리팩토링

--- src/main/java/com/example/paymentsapi/service/PaymentOrderService.java
+++ src/main/java/com/example/paymentsapi/service/PaymentOrderService.java
@@ -20,75 +20,74 @@ public class PaymentOrderService {
     private final OrderRepository orderRepository;
     private final CardRepository cardRepository;
 
+    private static final String SUCCESS_CODE = "200";
+    private static final String INVALID_EMAIL_CODE = "4003";
+    private static final String BELOW_ZERO_AMOUNT_CODE = "4002";
+    private static final String DUPLICATED_ORDER_ID_CODE = "4091";
+    private static final String INVALID_CARD_COMPANY_CODE = "4001";
+
+    private static final String SUCCESS_MSG = "결제성공";
+    private static final String INVALID_EMAIL_MSG = "이메일 주소 형식에 맞지 않습니다.";
+    private static final String BELOW_ZERO_AMOUNT_MSG = "금액은 0보다 커야 합니다.";
+    private static final String DUPLICATED_ORDER_ID_MSG = "이미 승인 및 취소가 진행된 중복된 주문번호 입니다. 다른 주문번호로 진행해주세요.";
+    private static final String INVALID_CARD_COMPANY_MSG = "유효하지 않은 카드사입니다.";
+
     public boolean isEmailValid(String email) {
-        // 이메일 형식을 확인하는 정규표현식
         String emailRegex = "^[a-zA-Z0-9_+&*-]+(?:\\.[a-zA-Z0-9_+&*-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,7}$";
         Pattern pattern = Pattern.compile(emailRegex);
         Matcher matcher = pattern.matcher(email);
-
         return matcher.matches();
     }
 
     public List<Order> OrderInfoSave(OrderStatusDto orderStatusDto) {
-
-        Integer Period = orderStatusDto.getPayPeriod(); //할부 기간
-        String StateCode = "200";
-        String StateMsg = "결제성공";
+        Integer Period = orderStatusDto.getPayPeriod();
+        String StateCode = SUCCESS_CODE;
+        String StateMsg = SUCCESS_MSG;
         String Transactionkey = "";
         String Email = orderStatusDto.getEmail();
         String CardName = orderStatusDto.getPaymentMethod();
-        if(Period == null){
+
+        if (Period == null) {
             Period = 0;
         }
-        /*
-        * 실제 결제 시에는 해당 부분에서 카드사와 연결하여 해당 금액 및 정보를 비교하고 리턴값을 받음
-        * 하지만 개인 프로젝트 상에서 카드사 연결이 불가하여 임의 디폴드값을 부여하고 메세지 노출
-        *
-        * */
-        
+
         if (!isEmailValid(Email)) {
-            //INVALID_EMAIL
-            StateCode = "4003";
-            StateMsg = "이메일 주소 형식에 맞지 않습니다.";
+            StateCode = INVALID_EMAIL_CODE;
+            StateMsg = INVALID_EMAIL_MSG;
         }
-        if(orderStatusDto.getAmount() <= 0){
-            //BELOW_ZERO_AMOUNT
-            StateCode = "4002";
-            StateMsg = "금액은 0보다 커야 합니다.";
+
+        if (orderStatusDto.getAmount() <= 0) {
+            StateCode = BELOW_ZERO_AMOUNT_CODE;
+            StateMsg = BELOW_ZERO_AMOUNT_MSG;
         }
 
         Boolean ChkOrderNo = orderRepository.findByOrderNo(orderStatusDto.getOrderNo());
-        if(ChkOrderNo != null){
-            //DUPLICATED_ORDER_ID
-            StateCode = "4091";
-            StateMsg = "이미 승인 및 취소가 진행된 중복된 주문번호 입니다. 다른 주문번호로 진행해주세요.";
+        if (ChkOrderNo != null) {
+            StateCode = DUPLICATED_ORDER_ID_CODE;
+            StateMsg = DUPLICATED_ORDER_ID_MSG;
         }
 
-        if(cardRepository.findByCardNameEN(orderStatusDto.getPaymentMethod()) == "" || orderStatusDto.getPaymentMethod() == ""){
-            //INVALID_CARD_COMPANY
-            StateCode = "4001";
-            StateMsg = "유효하지 않은 카드사입니다.";
+        if (cardRepository.findByCardNameEN(orderStatusDto.getPaymentMethod()).isEmpty() || orderStatusDto.getPaymentMethod().isEmpty()) {
+            StateCode = INVALID_CARD_COMPANY_CODE;
+            StateMsg = INVALID_CARD_COMPANY_MSG;
         }
 
-        //결제 실패저장은 결제 카드관련된 실패일 경우에만 저장(한도초과, 정지카드 등)
-        //결제 정보 등록 및 리턴값 부여
         Order orderInfo = Order.builder()
-                .userid(orderStatusDto.getUserid()) // 구매자 아이디
-                .email(Email) //구매자 이메일
-                .amount(orderStatusDto.getAmount()) //구매금액
-                .orderNo(orderStatusDto.getOrderNo()) //주문서번호(고유번호)
-                .resultStatus(StateMsg) // 결제 상태 내용
-                .resultStatusCode(StateCode) // 결제 상태 코드
-                .transactionkey(Transactionkey) //결제 고유 식별자
-                .orderDate(LocalDateTime.now()) //결제 시간
-                .paymentMethod(CardName) // 카드사명
-                .paymentCode(orderStatusDto.getPaymentCode()) //카드사 코드
-                .payPeriod(Period) //할부 기간
+                .userid(orderStatusDto.getUserid())
+                .email(Email)
+                .amount(orderStatusDto.getAmount())
+                .orderNo(orderStatusDto.getOrderNo())
+                .resultStatus(StateMsg)
+                .resultStatusCode(StateCode)
+                .transactionkey(Transactionkey)
+                .orderDate(LocalDateTime.now())
+                .paymentMethod(CardName)
+                .paymentCode(orderStatusDto.getPaymentCode())
+                .payPeriod(Period)
                 .build();
 
         Order savedOrder = orderRepository.save(orderInfo);
 
-        // 리스트가 필요한 경우, 저장된 주문을 포함한 리스트를 만들 수 있습니다.
         List<Order> searchResult = Collections.singletonList(savedOrder);
 
         return searchResult;
--- src/main/java/com/example/paymentsapi/service/ProduceKeyService.java
+++ src/main/java/com/example/paymentsapi/service/ProduceKeyService.java
@@ -8,11 +8,9 @@ import com.example.paymentsapi.web.dto.CommonDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+
 import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
 
 @RequiredArgsConstructor
 @Service
@@ -20,70 +18,60 @@ public class ProduceKeyService {
 
     private final CompanyRepository companyRepository;
     private final SerialkeyRepository serialkeyRepository;
-    public CommonDto insertCompany(String company) {
 
-        //회사명 중복 체크
-       String dupCompany=  companyRepository.findBycompanyName(company);
+    private static final String SUCCESS_MESSAGE = "성공";
+    private static final String DUPLICATE_COMPANY_MESSAGE = "회사명이 중복되어있습니다.";
+    private static final String KEY_ISSUE_SUCCESS_MESSAGE = "키발급 성공";
+    private static final String KEY_ISSUE_FAIL_MESSAGE = "키발급 실패";
 
-       if(!dupCompany.isEmpty()){
-           return CommonDto.builder()
-                   .message("회사명이 중복되어있습니다.")
-                   .status("fail")
-                   .httpStatus(HttpStatus.valueOf("fail"))
-                   .data("")
-                   .build();
+    public CommonDto insertCompany(String companyName) {
+        // 회사명 중복 체크
+        Optional<Company> existingCompany = companyRepository.findByCompanyName(companyName);
 
-       }
-        Company companyInfo = Company.builder()
-                .companyName(company)
+        if (existingCompany.isPresent()) {
+            return CommonDto.builder()
+                    .message(DUPLICATE_COMPANY_MESSAGE)
+                    .status("fail")
+                    .httpStatus(HttpStatus.BAD_REQUEST)
+                    .data("")
+                    .build();
+        }
+
+        Company newCompany = Company.builder()
+                .companyName(companyName)
                 .joinDate(LocalDateTime.now())
                 .build();
 
-        companyRepository.save(companyInfo);
+        companyRepository.save(newCompany);
 
         return CommonDto.builder()
-                .message("성공")
+                .message(SUCCESS_MESSAGE)
                 .status("success")
                 .httpStatus(HttpStatus.OK)
                 .data("")
                 .build();
-
     }
 
     public CommonDto insertKey(Integer companyCode) {
-        String message = "";
-        String status = "";
         Optional<Company> company = companyRepository.findById(companyCode);
 
-        if(company == null){
-        //키가 없는경우 키 발급
-            String inputString = "ABCDEFGHIJKLMNOPQRSTUVWXYMZ1234567890";
+        String message;
+        String status;
 
-            // 문자열을 문자 리스트로 변환
-            List<Character> charList = new ArrayList<>();
-            for (char c : inputString.toCharArray()) {
-                charList.add(c);
-            }
-            // 리스트를 랜덤하게 섞음
-            Collections.shuffle(charList);
-            // 섞인 리스트를 다시 문자열로 변환
-            StringBuilder resultStringBuilder = new StringBuilder();
-            for (char c : charList) {
-                resultStringBuilder.append(c);
-            }
-
-            String resultString = resultStringBuilder.toString();
-
+        if (!company.isPresent()) {
+            // 키 발급
+            String generatedKey = generateRandomKey();
             Serialkey serialkey = Serialkey.builder()
-                    .serialKey(resultString)
+                    .serialKey(generatedKey)
                     .companyCode(companyCode)
                     .build();
             serialkeyRepository.save(serialkey);
-            message = "키발급 성공";
+
+            message = KEY_ISSUE_SUCCESS_MESSAGE;
             status = "success";
-        }else{
-        //해당 코드에 키값이 있는경우 fail
-            message = "키발급 실패";
+        } else {
+            // 해당 코드에 키값이 있는 경우 fail
+            message = KEY_ISSUE_FAIL_MESSAGE;
             status = "fail";
         }
 
@@ -92,5 +80,23 @@ public class ProduceKeyService {
                 .status(status)
                 .data("")
                 .build();
+    }
+
+    private String generateRandomKey() {
+        String inputString = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+        List<Character> charList = new ArrayList<>();
+
+        for (char c : inputString.toCharArray()) {
+            charList.add(c);
+        }
+
+        Collections.shuffle(charList);
+
+        StringBuilder resultStringBuilder = new StringBuilder();
+        for (char c : charList) {
+            resultStringBuilder.append(c);
+        }
+
+        return resultStringBuilder.toString();
     }
 }
