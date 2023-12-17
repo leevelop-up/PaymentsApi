5c72abbb7749e7aa3badad5375710d11403a613c
LeeDonghyun <63052631+leevelop-up@users.noreply.github.com>
2023-12-17 22:57:23 +0900

    검색 기능 추가

--- src/main/java/com/example/paymentsapi/controller/PayListController.java
+++ src/main/java/com/example/paymentsapi/controller/PayListController.java
@@ -6,8 +6,12 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @RestController
@@ -19,10 +23,42 @@ public class PayListController {
     private final PayListService payListService;
     @GetMapping("/getlist")
     @ResponseBody
-    public Page<Order> getPayList(@RequestParam(defaultValue = "0") int page,
-                                  @RequestParam(defaultValue = "10") int size) {
+    public Page<Order> getPayList(@PageableDefault(page = 0, size = 10) Pageable pageable,
+                                  @RequestParam(required = false) String date,
+                                  @RequestParam(required = false) String userId,
+                                  @RequestParam(required = false) String orderNo,
+                                  @RequestParam(required = false) String resultStatus,
+                                  @RequestParam(required = false) String paymentMethod) {
+        Specification<Order> spec = Specification.where(null);
 
-        return payListService.getPayList(PageRequest.of(page, size));
+        if (date != null && !date.isEmpty()) {
+            // 예제로 날짜는 정확한 매칭으로 처리되도록 했습니다. 실제로는 범위 등을 고려해야 할 수 있습니다.
+            LocalDate parsedDate = LocalDate.parse(date);
+            spec = spec.and((root, query, criteriaBuilder) ->
+                    criteriaBuilder.equal(root.get("orderDate"), parsedDate));
+        }
+
+        if (userId != null && !userId.isEmpty()) {
+            spec = spec.and((root, query, criteriaBuilder) ->
+                    criteriaBuilder.equal(root.get("userId"), userId));
+        }
+
+        if (orderNo != null && !orderNo.isEmpty()) {
+            spec = spec.and((root, query, criteriaBuilder) ->
+                    criteriaBuilder.equal(root.get("orderNo"), orderNo));
+        }
+
+        if (resultStatus != null && !resultStatus.isEmpty()) {
+            spec = spec.and((root, query, criteriaBuilder) ->
+                    criteriaBuilder.equal(root.get("orderStatus"), resultStatus));
+        }
+
+        if (paymentMethod != null && !paymentMethod.isEmpty()) {
+            spec = spec.and((root, query, criteriaBuilder) ->
+                    criteriaBuilder.equal(root.get("paymentMethod"), paymentMethod));
+        }
+
+        return payListService.getPayList(spec, pageable);
     }
 
 }
--- src/main/java/com/example/paymentsapi/repository/order/OrderRepository.java
+++ src/main/java/com/example/paymentsapi/repository/order/OrderRepository.java
@@ -1,9 +1,14 @@
 package com.example.paymentsapi.repository.order;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 public interface OrderRepository extends JpaRepository<Order,Long> {
     List<Order> findByOrderNo(Integer orderNo);
+
+    Page<Order> findAll(Specification<Order> spec, Pageable pageable);
 }
